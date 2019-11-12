fa181e18b8c8f9a32f3ba266fb33c9edd72d94bc
Anders Høgh <andershogh@Anderss-MacBook-Pro-3.local>
2019-11-12 23:03:56 +0100

    Updates

---
+++ precache-manifest.8ea27f86d1d89a106061735ddd2039b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf3bd71d7e941127b97e2eea6d7404a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a652e0eafee47eca875d",
+    "url": "/robofriends/static/css/main.0273ea80.chunk.css"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robofriends/static/js/2.2a21f297.chunk.js"
+  },
+  {
+    "revision": "a652e0eafee47eca875d",
+    "url": "/robofriends/static/js/main.251f8939.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  }
+]);
